35853f53815b934ed700e153b717ff322242c2a0
DeividasRo <slenderiss@gmail.com>
2023-09-01 16:07:33 +0300

    Migration to skeleton 2.0 + some css changes

--- src/lib/GuessCard.svelte
+++ src/lib/GuessCard.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
-
 	export let guess: Guess;
 </script>
 
-<section class="card flex w-full select-none justify-between p-3">
-	<p class="text-lg font-bold">{guess.name}</p>
+<section class="card card-hover flex w-full select-none justify-between px-3 py-2">
+	<p class="text-xl font-bold">{guess.name}</p>
 	<p class="text-lg font-bold">{guess.distance} km</p>
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,11 +10,20 @@
 
 <div class="container mx-auto flex h-full flex-col items-center justify-start">
 	<img class="max-w-md pt-20" src={LTMap} alt="Map of Lithuania" />
-	<input class="input my-10 max-w-xl" />
+	<input class="input my-10 h-12 max-w-md p-4 text-center text-xl drop-shadow-xl" />
+
 	<!-- Dynamically increasing list of guesses below the input field -->
-	<ul class="list w-9/12 max-w-xl space-y-2">
+	<ul class="list w-7/12 max-w-sm space-y-3">
 		{#each guesses as guess}
 			<GuessCard {guess} />
 		{/each}
 	</ul>
 </div>
+
+<!--
+	TODO:
+	Display guess dynamically
+	Dropdown selection menu for available guesses
+	Try to implement guessed cities onto the map
+	
+-->
