d13c0e13a55ceeb58fda66c30a46e77c9aa96da1
DeividasRo <slenderiss@gmail.com>
2023-09-06 00:38:42 +0300

    popup autocomplete input implemented

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 </script>
 
 <svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { Autocomplete } from '@skeletonlabs/skeleton';
-	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
+	import { Autocomplete, popup } from '@skeletonlabs/skeleton';
+	import type { AutocompleteOption, PopupSettings } from '@skeletonlabs/skeleton';
 	import GuessCard from '../lib/components/GuessCard.svelte';
 	import LTMap from '$lib/assets/lt.svg';
 	import { resetGuessStore, addToGuessStore, guessStore } from '$lib/stores/guess-store';
@@ -9,7 +9,6 @@
 
 	let currentGuess: string = '';
 	let correctGuess: string = 'Vilnius';
-	let valueSingle: string = 'books';
 
 	function getCityData(cityName: string) {
 		const cityPosition = cities.findIndex((city) => {
@@ -30,21 +29,52 @@
 		addToGuessStore(currentGuess, Math.round(distance));
 		currentGuess = '';
 	}
+
+	console.log(cities.map((value) => value.city));
+	let cityNames: string[] = cities.map((value) => value.city).sort();
+
+	const flavorOptions: AutocompleteOption[] = [];
+
+	cityNames.forEach((cityName) => flavorOptions.push({ label: cityName, value: cityName }));
+
+	function onFlavorSelection(event: CustomEvent<AutocompleteOption>): void {
+		currentGuess = event.detail.label;
+	}
+
+	let popupSettings: PopupSettings = {
+		event: 'focus-click',
+		target: 'popupAutocomplete',
+		placement: 'bottom'
+	};
 </script>
 
 <div class="container mx-auto flex h-full flex-col items-center justify-start">
 	<img class="max-w-md pt-20" src={LTMap} alt="Map of Lithuania" />
 	<input
-		list="cities"
-		class="input mt-10 h-12 max-w-md p-4 text-center text-xl drop-shadow-xl"
+		type="search"
+		name="autocomplete-search"
+		class="input my-10 h-12 max-w-md p-4 text-center text-xl outline-none drop-shadow-xl"
 		placeholder="Enter a city in Lithuania..."
 		bind:value={currentGuess}
+		use:popup={popupSettings}
 		on:keydown={(e) => {
 			if (e.key === 'Enter') {
+				console.log(e.key);
 				handleInput();
 			}
 		}}
 	/>
+	<div
+		class="card -mt-2 max-h-48 w-full max-w-sm overflow-y-auto rounded-none rounded-b-lg p-2 font-bold"
+		tabindex="-1"
+		data-popup="popupAutocomplete"
+	>
+		<Autocomplete
+			bind:input={currentGuess}
+			options={flavorOptions}
+			on:selection={onFlavorSelection}
+		/>
+	</div>
 
 	<!-- Dynamically increasing list of guesses below the input field -->
 	<ul class="list w-7/12 max-w-sm space-y-3">
@@ -60,7 +90,9 @@
 
 <!--
 	TODO:
-	- Dropdown selection menu for available guesses
+	- Put input stuff into a component
+	- Game loop, select correct word with global timer
+	- Styling, colors fixes
 	- Auto focus on keydown
 	- Try to implement guessed cities onto the map
 	- Guess list and correct guess animations
