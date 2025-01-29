f5dc20b23b53b08a0df1906320213e9a699af076
DeividasRo <slenderiss@gmail.com>
2025-01-29 10:42:16 +0200

    translations + small adjustments

--- src/lib/components/History/Game.svelte
+++ src/lib/components/History/Game.svelte
@@ -3,6 +3,7 @@
 	import { fly } from 'svelte/transition';
 	import { quintOut } from 'svelte/easing';
 	import GuessCard from '$lib/components/History/GuessCard.svelte';
+	import GuessInput from '$lib/components/History/GuessInput.svelte';
 	import {
 		resetGuessesStore,
 		addToGuessesStore,
@@ -14,7 +15,6 @@
 	import { startDateStore, savedDateStore, updateSavedDateStore } from '$lib/stores/dateStore';
 	import { languageStore } from '$lib/stores/languageStore';
 	import languagesJson from '$lib/assets/languages.json';
-	import GuessInput from '$lib/components/History/GuessInput.svelte';
 
 	let currentGuess: string = '';
 	let maxGuessCount: number = 7;
@@ -65,10 +65,14 @@
 </script>
 
 <div class="container mx-auto flex h-full flex-col items-center justify-start caret-transparent">
-	{#if $gameStateStore == 'in progress' && browser}
-		<div class="h3 mb-2 mt-14 max-w-lg text-center font-semibold">
+	<div class="card h3 mb-2 mt-10 max-w-lg p-4 text-center font-bold drop-shadow-md">
+		{#if $languageStore == 'lithuania'}
+			{getEventData($answerStore).event_lt}
+		{:else if $languageStore == 'united-kingdom'}
 			{getEventData($answerStore).event}
-		</div>
+		{/if}
+	</div>
+	{#if $gameStateStore == 'in progress' && browser}
 		<GuessInput bind:inputValue={currentGuess} placeholderText="2025" on:input={handleInput} />
 	{:else if $gameStateStore === 'won' && browser}
 		<div
@@ -81,9 +85,9 @@
 				easing: quintOut
 			}}
 		>
-			<p class=" my-5 h-12 text-center text-xl sm:my-8 sm:text-2xl">
+			<p class="my-8 h-12 text-center text-xl sm:text-2xl">
 				{@html languagesJson[$languageStore]['won-message']}<span class="font-bold"
-					>{$answerStore} m.
+					>{$answerStore} {@html languagesJson[$languageStore]['year-short']}
 				</span>
 			</p>
 		</div>
@@ -98,15 +102,17 @@
 				easing: quintOut
 			}}
 		>
-			<p class="my-5 h-12 text-center text-xl sm:my-9 sm:text-2xl">
+			<p class="my-8 h-12 text-center text-xl sm:text-2xl">
 				{@html languagesJson[$languageStore]['lost-message']}
-				<span class="font-bold">{$answerStore} m.</span>
+				<span class="font-bold"
+					>{$answerStore} {@html languagesJson[$languageStore]['year-short']}</span
+				>
 			</p>
 		</div>
 	{/if}
 
 	<!-- Dynamically increasing list of guesses below the input field -->
-	<ul class="list mx-auto mb-4 w-60 max-w-md space-y-3">
+	<ul class="list mx-auto my-4 w-52 max-w-md space-y-3">
 		{#each $guessesStore as guess}
 			<div
 				in:fly={{
--- src/lib/components/History/GuessCard.svelte
+++ src/lib/components/History/GuessCard.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+	import { languageStore } from '$lib/stores/languageStore';
+	import languagesJson from '$lib/assets/languages.json';
 	export let guess: HistoryGuess;
 </script>
 
 <section class="card card-hover flex max-w-sm select-none justify-between px-4 py-2">
-	<p class="text-xl font-bold">{guess.year} m.</p>
+	<p class="text-xl font-bold">{guess.year} {@html languagesJson[$languageStore]['year-short']}</p>
 	<p>
 		<!-- Change to up/down arrow icon -->
 		{#if guess.difference > 0}
